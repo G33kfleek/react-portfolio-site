503b80635d358a8787e7d342c70f8cd6d4ffe954
G33kfleek <104208543+G33kfleek@users.noreply.github.com>
2023-05-25 23:49:23 +0300

    2 files modified

--- src/components/DefaultButton.js
+++ src/components/DefaultButton.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import "../stylesheets/DefButton.css"
+import '../stylesheets/DefButton.css';
+import { Link } from 'react-router-dom';
+
 const DefButton = ({ icon, label, onClick }) => {
   return (
-    <button className="def-button" onClick={onClick}>
-      <span className="icon">{icon}</span>
-      <span className="label">{label}</span>
-    </button>
+    <Link to={to} className="icon-button">
+      <button className="def-button" onClick={onClick}>
+        <span className="icon">{icon}</span>
+        <span className="label">{label}</span>
+      </button>
+    </Link>
   );
 };
 
